58866f8ade10f92f0e6e3fdd661f57a0e37c155e
kamilvlcek <kamil@biomed.cas.cz>
2023-04-21 16:20:07 +0200

    Edo - zmeny pro novy typ souboru SpaNav ze SleepForest.js
    poslal mi 14. duben 2023 16:21:03 jako ReadTR3c.m

--- ReadTR3.m
+++ ReadTR3.m
@@ -1,4 +1,4 @@
-function out=ReadTR3(FileName)
+function out=ReadTR3(FileNameIn)
 
 %compute proportion of real path length to optimal one
 %categorize tasks according to the demands/difficulty level
@@ -7,7 +7,7 @@ function out=ReadTR3(FileName)
 
 PlotsInFigure=15;
 
-out{1,1}=FileName;
+out{1,1}=FileNameIn;
 out{2,1}='trial';
 out{2,2}='aim';
 out{2,3}='animal';
@@ -26,7 +26,9 @@ out{2,15}='angle real';
 out{2,16}='angle error';
 out{2,17}='trial category';
 
-FileName=['d:\prace\mff\data\aappSeg\skriptyForest\output\' FileName ];
+
+FileName=['D:\Users\kelemen\Data\VRKamil\' FileNameIn '.tr'];
+
 
 FileID=fopen(FileName);
 
@@ -34,29 +36,26 @@ SearchNum=0;
 NL=0;line=[];
 %while strcmp(line(1:6),' 0.000')==0
 while isempty(strfind(line, 'Ukaz na'));
-    line=fgetl(FileID);
+    line=fgetl(FileID);  %%%
     NL=NL+1;
     if strfind(line, 'Aim position')
-        Aim=GetAimPositions(line);
-        AimX=Aim{1};
-        AimY=Aim{2};
+        Aim=GetAimPositionsb(line); 
+        AimX=Aim{1}; 
+        AimY=Aim{2}; 
     end
+    if strfind(line,'Avatar location changed:')     %%%
+        ba=strfind(line, '[');
+        bb=strfind(line, ',');
+        bc=strfind(line, ']');
+        StartLocX=str2num(line(ba+1:bb-1));
+        StartLocY=str2num(line(bb+2:bc-1));
+    end                                             %%%
 end
 n=strfind(line, 'text:Najdi');
 Cil=line(n+11:end-2);
 DLN=0;
 firstdataline=0;
 NumErr=0;
-
-X(1)=mean(AimX(1,:));Y(1)=mean(AimY(1,:));
-X(2)=mean(AimX(2,:));Y(2)=mean(AimY(2,:));
-X(3)=mean(AimX(3,:));Y(3)=mean(AimY(3,:));
-X(4)=mean(AimX(4,:));Y(4)=mean(AimY(4,:));
-X(5)=mean(AimX(5,:));Y(5)=mean(AimY(5,:));
-X(6)=mean(AimX(6,:));Y(6)=mean(AimY(6,:));        
-X(7)=mean(AimX(7,:));Y(7)=mean(AimY(7,:));
-X(8)=mean(AimX(8,:));Y(8)=mean(AimY(8,:));
-X(9)=mean(AimX(9,:));Y(9)=mean(AimY(9,:));
 
 %figure
 
@@ -66,7 +65,7 @@ while feof(FileID)==0
     if strcmp(line(1),' ')
         DLN=DLN+1;
         if firstdataline==0
-            divider=line(7);
+            divider=line(7); 
             firstdataline=1;
         end
         k=strfind(line, divider);
@@ -81,6 +80,13 @@ while feof(FileID)==0
             Angle=Angle+360;
         end
     end 
+    if strfind(line,'Avatar location changed:')     %%%
+        ba=strfind(line, '[');
+        bb=strfind(line, ',');
+        bc=strfind(line, ']');
+        StartLocX=str2num(line(ba+1:bb-1));
+        StartLocY=str2num(line(bb+2:bc-1));
+    end                                             %%%
     if strfind(line, 'text:Najdi')
         time=[];
         ArenaLocX=[];
@@ -92,39 +98,39 @@ while feof(FileID)==0
         n=strfind(line, 'text:Najdi');
         Cil=line(n+11:end-2);
     end 
-    if strfind(line, 'Avoid entrance:')
-        NumErr=NumErr+1;
-        n=strfind(line, 'Avoid entrance:');
-        ErrAim=line(n+15:n+19);
-        if strcmp(ErrAim(4),'A')
-            ErrBox(NumErr)=1;
-        end
-        if strcmp(ErrAim(4),'B')
-            ErrBox(NumErr)=2;
-        end
-        if strcmp(ErrAim(4),'C')
-            ErrBox(NumErr)=3;
-        end
-        if strcmp(ErrAim(4),'D')
-            ErrBox(NumErr)=4;
-        end
-        if strcmp(ErrAim(4),'E')
-            ErrBox(NumErr)=5;
-        end
-        if strcmp(ErrAim(4),'F')
-            ErrBox(NumErr)=6;
-        end        
-        if strcmp(ErrAim(4),'G')
-            ErrBox(NumErr)=7;
-        end
-        if strcmp(ErrAim(4),'H')
-            ErrBox(NumErr)=8;
-        end
-        if strcmp(ErrAim(4),'I')
-            ErrBox(NumErr)=9;
-        end        
-        ErrGoal(NumErr)=str2num(ErrAim(5));
-    end
+%     if strfind(line, 'Avoid entrance:') %%%
+%         NumErr=NumErr+1;
+%         n=strfind(line, 'Avoid entrance:');
+%         ErrAim=line(n+15:n+19);
+%         if strcmp(ErrAim(4),'A')
+%             ErrBox(NumErr)=1;
+%         end
+%         if strcmp(ErrAim(4),'B')
+%             ErrBox(NumErr)=2;
+%         end
+%         if strcmp(ErrAim(4),'C')
+%             ErrBox(NumErr)=3;
+%         end
+%         if strcmp(ErrAim(4),'D')
+%             ErrBox(NumErr)=4;
+%         end
+%         if strcmp(ErrAim(4),'E')
+%             ErrBox(NumErr)=5;
+%         end
+%         if strcmp(ErrAim(4),'F')
+%             ErrBox(NumErr)=6;
+%         end        
+%         if strcmp(ErrAim(4),'G')
+%             ErrBox(NumErr)=7;
+%         end
+%         if strcmp(ErrAim(4),'H')
+%             ErrBox(NumErr)=8;
+%         end
+%         if strcmp(ErrAim(4),'I')
+%             ErrBox(NumErr)=9;
+%         end        
+%         ErrGoal(NumErr)=str2num(ErrAim(5));
+%     end
 
     
     
@@ -134,16 +140,16 @@ while feof(FileID)==0
         
         if strfind(line, 'Aim entrance:')
             n=strfind(line, 'Aim entrance:');
-            CurrentAim=line(n+13:n+17);
+            CurrentAim=line(n+13:n+17); %%%
             AimFound=1;
         end
         if strfind(line, 'Aim not found:')
             n=strfind(line, 'Aim not found:');
-            CurrentAim=line(n+14:n+18);
+            CurrentAim=line(n+14:n+18); %%%
             AimFound=0;
         end
         if strcmp(CurrentAim(4),'A')
-            CurBox=1;
+            CurBox=1; %%%
         end
         if strcmp(CurrentAim(4),'B')
             CurBox=2;
@@ -168,45 +174,51 @@ while feof(FileID)==0
         end
         if strcmp(CurrentAim(4),'I')
             CurBox=9;
-        end        
+        end      
+%         CurrentAim %%%
+%         CurrentAim(4) %%%
+%         CurrentAim(5) %%%
         CurGoal=str2num(CurrentAim(5));
+%         CurGoal %%%
         %figure 
         if rem(SearchNum,PlotsInFigure)==1
             figure
         end
         PlotPosition=SearchNum;
-        while PlotPosition>PlotsInFigure; 
+        while PlotPosition>PlotsInFigure 
             PlotPosition=PlotPosition-PlotsInFigure;
         end
         subplot(3,5,PlotPosition)
-        plot([X(1)+500 X(2)-500],[0-Y(1) 0-Y(2)],'k')
+        plot([AimX(1)+500 AimX(2)-500],[0-AimY(1) 0-AimY(2)],'k') %%%
         hold on
-        plot([X(2)+500 X(3)-500],[0-Y(2) 0-Y(3)],'k')
+        plot([AimX(2)+500 AimX(3)-500],[0-AimY(2) 0-AimY(3)],'k')
         hold on
-        plot([X(3) X(6)],[0-Y(3)-500 0-Y(6)+500],'k')
+        plot([AimX(3) AimX(6)],[0-AimY(3)-500 0-AimY(6)+500],'k')
         hold on
-        plot([X(6)-500 X(5)+500],[0-Y(6) 0-Y(5)],'k')
+        plot([AimX(6)-500 AimX(5)+500],[0-AimY(6) 0-AimY(5)],'k')
         hold on
-        plot([X(5)-500 X(4)+500],[0-Y(5) 0-Y(4)],'k')
+        plot([AimX(5)-500 AimX(4)+500],[0-AimY(5) 0-AimY(4)],'k')
         hold on
-        plot([X(4) X(7)],[0-Y(4)-500 0-Y(7)+500],'k')
+        plot([AimX(4) AimX(7)],[0-AimY(4)-500 0-AimY(7)+500],'k')
         hold on
-        plot([X(7)+500 X(8)-500],[0-Y(7) 0-Y(8)],'k')
+        plot([AimX(7)+500 AimX(8)-500],[0-AimY(7) 0-AimY(8)],'k')
         hold on
-        plot([X(8)+500 X(9)-500],[0-Y(8) 0-Y(9)],'k')
+        plot([AimX(8)+500 AimX(9)-500],[0-AimY(8) 0-AimY(9)],'k')
         hold on
         plot(ArenaLocX(2:end),0-ArenaLocY(2:end),'b')%%
-        StartEndField=DetStartEndField(ArenaLocX(2:end),ArenaLocY(2:end),X,Y); %find start and end field
+%         ArenaLocX %%%
+%         X %%%
+        StartEndField=DetStartEndField(ArenaLocX(2:end),ArenaLocY(2:end),AimX,AimY); %find start and end field
         TrialType=DetTrialType([StartEndField(1) CurBox]);  %determine the type of test trial 
         for box=1:9
-          for i=1:6
-             hold on
-             plot(AimX(box,i),0-AimY(box,i), '.k')
-          end
+%           for i=1:6
+%              hold on
+             plot(AimX(box),0-AimY(box), '.k')
+%           end
         end
 
         hold on
-        plot(AimX(CurBox,CurGoal),0-AimY(CurBox,CurGoal), '.g')
+        plot(AimX(CurBox),0-AimY(CurBox), '.g') %%%
         for i=1:NumErr
              hold on
              plot(AimX(ErrBox(i),ErrGoal(i)),0-AimY(ErrBox(i),ErrGoal(i)), '.r') 
@@ -224,9 +236,16 @@ while feof(FileID)==0
         axis off
         Duration=time(end)-time(2);
         Length=LengthofTrack(ArenaLocX(2:end),ArenaLocY(2:end));
-        ToOptimalLength=Length/dist(ArenaLocX(2),ArenaLocY(2),AimX(CurBox,CurGoal),AimY(CurBox,CurGoal));
+%         ArenaLocX(2) %%%
+%         ArenaLocY(2) %%%
+%         CurBox %%%
+%         CurGoal %%%
+%         AimX(CurBox,CurGoal) %%%
+%         AimY(CurBox,CurGoal) %%%
+        ToOptimalLength=Length/dist(ArenaLocX(2),ArenaLocY(2),ArenaLocX(DLN),ArenaLocY(DLN));  %%%
+%       ToOptimalLength=Length/dist(ArenaLocX(2),ArenaLocY(2),AimX(CurBox,CurGoal),AimY(CurBox,CurGoal));  %%%
         %ToOptimalLength=Length/dist(ArenaLocX(2),ArenaLocY(2),ArenaLocX(end),ArenaLocY(end));
-        RealAngle=XY2ang(AimX(CurBox,CurGoal)-ArenaLocX(2),AimY(CurBox,CurGoal)-ArenaLocY(2))/(2*3.141592653)*360;
+        RealAngle=XY2ang(ArenaLocX(DLN)-ArenaLocX(2),ArenaLocY(DLN)-ArenaLocY(2))/(2*3.141592653)*360;
         %RealAngle=XY2ang(ArenaLocX(end)-ArenaLocX(2),ArenaLocY(end)-ArenaLocY(2))/(2*3.141592653)*360;
         AngleError=Angle-RealAngle;
         if AngleError>180
@@ -236,7 +255,11 @@ while feof(FileID)==0
             AngleError=AngleError+360;
         end
         %title(['Search# ' num2str(SearchNum) '   ' CurrentAim '-' Cil '   duration: ' num2str(Duration) '   length: ' num2str(Length) '   Errors: ' num2str(NumErr)])
-        title([num2str(SearchNum)])
+        if SearchNum==1
+            title([FileNameIn ' ' num2str(SearchNum)])
+        else
+            title([num2str(SearchNum)])
+        end
         out{2+SearchNum,1}=SearchNum;
         out{2+SearchNum,2}=CurrentAim;
         out{2+SearchNum,3}=Cil;
